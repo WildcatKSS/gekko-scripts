aeb6639ee906acda4531e842ec2f733cdba0a548
Marvin <35631849+WildcatKSS@users.noreply.github.com>
2021-09-04 21:18:54 +0200

    Update gekko-service.sh

--- gekko-service.sh
+++ gekko-service.sh
@@ -1,1 +1,14 @@
+[Unit]
+Description=Job that runs your user script
 
+[Service]
+ExecStart=/some/command
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable foo.service
