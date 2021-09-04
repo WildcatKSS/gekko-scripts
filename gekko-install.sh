67abcf71b3fee2f19785f09a6e2ee6af98dc5836
Marvin <35631849+WildcatKSS@users.noreply.github.com>
2021-09-04 23:28:55 +0200

    Update gekko-install.sh

--- gekko-install.sh
+++ gekko-install.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
 
+# Installing the crypto trading bot Gekko and the additional strategie
+cd /usr/local/lib
+
+sudo apt-get update -y
+
+sudo apt-get install -y git nodejs npm
+
+sudo apt-get autoremove -y
+
+sudo git clone git://github.com/askmike/gekko.git -b stable
+
+sudo git clone https://github.com/WildcatKSS/gekko-strategies.git -b stable
+
+sudo cp gekko-strategies/* gekko -R
+
+cd gekko
+
+sudo npm install --only=production
+
+sudo npm install talib tulind convnetjs zero-fill stats-lite numbro mathjs cluster lodash.ismatch gauss
+
+cd exchange
+
+sudo npm install --only=production
+
+sudo npm install talib tulind convnetjs zero-fill stats-lite numbro mathjs cluster lodash.ismatch gauss
+
+# Edit crontab to run on a monthly basis an update of the crypto trading bot Gekko and the strategies
+sudo gekko-update.sh
+
+#Create a Gekko service and start gekko
+sudo gekko-service.sh
