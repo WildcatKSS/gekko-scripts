c5c45f773a539556fd78ea0cbf97e7eb1b6d6208
Marvin <35631849+WildcatKSS@users.noreply.github.com>
2021-09-04 23:43:37 +0200

    Update gekko-update.sh

--- gekko-update.sh
+++ gekko-update.sh
@@ -13,62 +13,18 @@ sudo git checkout stable
 
 sudo git pull
 
-cd ..
-
-cp gekko-strategies/* gekko -R
-
-
-
+sudo cp /usr/local/lib/gekko-strategies/* /usr/local/lib/gekko -R
 
 cd /usr/local/lib/gekko
 
-npm install --only=production
+sudo npm install --only=production
 
 sudo npm install talib tulind convnetjs zero-fill stats-lite numbro mathjs cluster lodash.ismatch gauss
 
-cd exchange
+cd /usr/local/lib/gekko/exchange
 
-npm install --only=production
+sudo npm install --only=production
 
 sudo npm install talib tulind convnetjs zero-fill stats-lite numbro mathjs cluster lodash.ismatch gauss
-
 
 exit 0
-
-
-
-
-#!/bin/bash
-
-# Installing the crypto trading bot Gekko and the additional strategie
-cd /usr/local/lib
-
-sudo apt-get update -y
-
-sudo apt-get install -y git nodejs npm
-
-sudo apt-get autoremove -y
-
-sudo git clone git://github.com/askmike/gekko.git -b stable
-
-sudo git clone https://github.com/WildcatKSS/gekko-strategies.git -b stable
-
-sudo cp gekko-strategies/* gekko -R
-
-cd gekko
-
-sudo npm install --only=production
-
-sudo npm install talib tulind convnetjs zero-fill stats-lite numbro mathjs cluster lodash.ismatch gauss
-
-cd exchange
-
-sudo npm install --only=production
-
-sudo npm install talib tulind convnetjs zero-fill stats-lite numbro mathjs cluster lodash.ismatch gauss
-
-# Edit crontab to run on a monthly basis an update of the crypto trading bot Gekko and the strategies
-sudo gekko-update.sh
-
-#Create a Gekko service and start gekko
-sudo gekko-service.sh
